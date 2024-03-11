e3167fcb695ad9f41a2dba721efbaf1d42944fc6
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-03-11 12:54:32 +0530

    Update 12_delegate_constructor.cpp

--- Modern_CPP_11_14_17_20/C++11_14/12_delegate_constructor.cpp
+++ Modern_CPP_11_14_17_20/C++11_14/12_delegate_constructor.cpp
@@ -1,6 +1,7 @@
 Delegate constructors: 
 C++11 introduces the concept of a delegate constructor, which allows a constructor to call another
-constructor in a constructor in the same class.
+constructor in a constructor in the same class. It's useful to call zero parameter constructor 
+to initialize other members of class.
 thus simplifying the code:
 
 #include <iostream>
