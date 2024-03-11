8e5c19a3a4e37a82d23416da0ca72c8f3fbbd035
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-03-11 13:00:40 +0530

    Update 1_auto_decltype.cpp

--- Modern_CPP_11_14_17_20/C++11_14/1_auto_decltype.cpp
+++ Modern_CPP_11_14_17_20/C++11_14/1_auto_decltype.cpp
@@ -80,3 +80,22 @@ int add(auto x, auto y): ->wrong
 
 auto can not be used for type derivation:
 auto auto_arr[10] = {arr};
+------------------------------------------------------
+decltype(auto):
+The decltype(auto) type-specifier also deduces a type like auto does. 
+However, it deduces return types while keeping their references and cv-qualifiers, 
+while auto will not.
+
+const int x = 0;
+auto x1 = x; // int
+decltype(auto) x2 = x; // const int
+int y = 0;
+int& y1 = y;
+auto y2 = y1; // int
+decltype(auto) y3 = y1; // int&
+int&& z = 0;
+auto z1 = std::move(z); // int
+decltype(auto) z2 = std::move(z); // int&&
+	
+
+
