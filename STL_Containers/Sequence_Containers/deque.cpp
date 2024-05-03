f086598d33ec2468e4580ff6faea4762b48fe99c
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-05-03 07:50:39 +0530

    Create deque.cpp

---
+++ STL_Containers/Sequence_Containers/deque.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include <deque>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	/****************Initializations***********************/
+		//initialize list with default values
+		//deque<int>dq(5);
+		
+		//initialize list with same values
+		//deque<int>dq(5, 10);
+		
+		//initialize list with initializer list
+		deque<int>dq = {10, 20, 30, 40, 20};
+				
+	/**************Add***********************************/
+		//push_back and push_front
+		//dq.push_front(11);
+		//dq.push_back(18);
+	      
+	/************Access****************************/
+      front() - returns the first element at front
+       back() - returns the last element at back
+       at() - returns element at the specified inde
+  	
+	/************Remove data*****************************/
+                 1) pop_front() - removes the element at the front
+                 2) pop_back() - removes the element at the back
+                    // remove the first element of the list
+                    numbers.pop_front();
+                    // remove the last element of the list
+                    numbers.pop_back();
+		 3) Erase elements: can be removed by direct passing the value.
+		    //erase element 30 
+		    int y = 20;
+		    lt.remove(y);
+		    for(auto it = lt.begin(); it != lt.end(); it++)
+		    {
+			cout<<"values in list:"<<*it<<endl;
+		    }
+		//Erase elements by position and position range
+		//lt.erase(lt.begin()); //by position but do not use like vec.begin()+1
+		/*for(auto it = lt.begin(); it != lt.end(); it++)
+		{
+			cout<<"values in list:"<<*it<<endl;
+		}*/
+	
+		//lt.erase(lt.begin(), lt.begin()); //by position range	
+		//iterate list 
+		/*for(auto it = lt.begin(); it != lt.end(); it++)
+		{
+			cout<<"values in list:"<<*it<<endl;
+		}*/
+	
+		//Find element:
+		/*
+		int x = 20;
+		auto it = find(lt.begin(), lt.end(), x);
+		if(it != lt.end())
+		{
+			cout<<*it<<endl;
+		}
+		else
+		{
+			cout<<"No found"<<endl;
+		}
+		*/
+	
+	
+	return 0;
+}
