cb3cb62c97b2e275bbbd1b5410ef8f7770ce7edc
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-07-12 18:07:00 +0530

    Update parseString.cpp

--- C++_concepts/String/parseString.cpp
+++ C++_concepts/String/parseString.cpp
@@ -1,1 +1,37 @@
+#include<iostream>
+#include <string>
 
+std::string checkTypeOfString(std::string s)
+{
+	bool isdigit = false;
+	bool isAlphabet = false;
+	for (auto ch : s)
+	{   
+		if (ch == '.')
+			return "float";
+		else if ((ch > 'A' && ch < 'Z') || (ch > 'a' && ch < 'z') )
+		{
+			isAlphabet = true;
+		}
+		else if (ch > '0' && ch < '9')
+		{
+			isdigit = true;
+		}
+	}
+	if(isAlphabet && isdigit)
+		return "Alphanumeric";
+	else if(isAlphabet)
+		return "Alphabet";
+	else
+		return "Numeric";
+}
+
+int main()
+{
+	std::string str("ABC12");
+		
+	std::string ret = checkTypeOfString(str);
+	std::cout << "Type is:" << ret << std::endl;
+		
+	return 0;
+}
