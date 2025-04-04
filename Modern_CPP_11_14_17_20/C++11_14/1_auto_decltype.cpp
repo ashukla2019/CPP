85216262859d2ee7b770d95906bc201c27a67003
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2025-04-04 16:36:12 +0530

    Update 1_auto_decltype.cpp

--- Modern_CPP_11_14_17_20/C++11_14/1_auto_decltype.cpp
+++ Modern_CPP_11_14_17_20/C++11_14/1_auto_decltype.cpp
@@ -1,10 +1,16 @@
 //Before C++11, the auto keyword was used for storage class.
 //auto is now a sort of placeholder for a type, telling the compiler it has to deduce the actual type 
 //of a variable that is being declared from its initializer. 
+1) Auto type deduction:
 	auto i = 42;        // i is an int
 	auto l = 42LL;      // l is an long long
 	auto p = new foo(); // p is a foo*
-	
+
+2) Type deduction with braced initializers (C++14)
+	auto A = { 1, 2 }; // std::initializer_list<int>
+	auto B = { 3 }; // std::initializer_list<int>
+	auto C{ 4 }; // int
+	auto D = { 5, 6.7 }; // C3535: cannot deduce type for 'auto' from initializer list'	
 	auto i = 10+5;
 	1) compile time: to fetch the result type of expression(10+5), which will be int.
 	2) run time: to evaluate the expression at run time and assign it to i. 
@@ -13,13 +19,13 @@
 	//auto retVal = gfun(45, 4.5f);
 	decltype(gfun(45, 4.5f)) retVal;
 
-	//auto can be used for iterators also.
+3) auto can be used for iterators also.
 	vector<int>::iterator it; //old way
 	for(auto it = begin(map); it != end(map); ++it)  -->auto it will automatically will take type
 	{
 	}
 
-	//Function return type deduction is implemented in C++14:
+4) Function return type deduction is implemented in C++14:
 	auto Correct(int i)
 	{
 	  if (i == 1)
@@ -27,13 +33,6 @@
 
 	  return Correct(i-1)+i;  // ok to call it now
 	}
-
-	//Type deduction with braced initializers (C++14)
-	auto A = { 1, 2 }; // std::initializer_list<int>
-	auto B = { 3 }; // std::initializer_list<int>
-	auto C{ 4 }; // int
-	auto D = { 5, 6.7 }; // C3535: cannot deduce type for 'auto' from initializer list'
-	
 	//Don't use auto: when type conversion is needed...
 
 //Decltype: It checks the type of expression
