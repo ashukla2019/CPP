45852f688bbf3e466d9b5398ecac97b875badc05
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2025-04-08 12:49:40 +0530

    Update 1_single_responsiblity.cpp

--- SOLID_Principal/1_single_responsiblity.cpp
+++ SOLID_Principal/1_single_responsiblity.cpp
@@ -1,53 +1,32 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <string>
+using namespace std;
+/*
+                    +------------------------+
+                    |   ios                   |
+                    +------------------------+
+                             |
+                             |
+              +---------------------------+
+              |                           |
+       +--------------+            +-----------------+
+       |  istream     |            |   ostream       |
+       +--------------+            +-----------------+
+            |                           |
+            |                           |
+    +---------------+          +-----------------+
+    |   ifstream    |          |    ofstream     |
+    +---------------+          +-----------------+
+            |                           |
+    +---------------+          +-----------------+
+    |   filebuf     |          |    filebuf      |
+    +---------------+          +-----------------+
+*/
 
-//Problematic code:Without using single responsibility principle, There would be 2 reason to modify implementation
-// one writeFile and other readFile
-class FileManager
-{
-	std::string fileName;
-	std::string data;
+class FileWriter {
 public:
-	FileManager(std::string fileName, std::string data) :fileName(fileName),data(data)
-	{
-
-	}
-	void writeFile()
-	{
-		std::ofstream fout;
-		fout.open(fileName);
-		fout << data;
-		fout.close();
-	}
-	void readFile()
-	{
-		std::ifstream fin;
-		fin.open(fileName);
-		std::string line;
-		std::vector<std::string> data;
-		while (getline(fin, line))
-		{
-			data.push_back(line);
-		}
-		fin.close();
-	}
-
-};
-int main()
-{
-	std::string data("ABC DEF ");
-	FileManager fm("data.txt", data);
-	fm.writeFile();
-	fm.readFile();
-	return 0;
-}
-
-Implementaiton using single responsibilty principle:
-class FileWriter 
-public:
-    void saveToFile(string filename, string data) {
+    void writeToFile(string filename, string data) {
         ofstream file(filename);
         file << data;
         file.close();
@@ -74,7 +53,7 @@ class FileManager : public FileReader, public FileWriter {
 
 int main() {
     FileManager file_manager;
-    file_manager.saveToFile("data.txt", "Hello, World!");
+    file_manager.writeToFile("data.txt", "Hello, World!");
 
     vector<string> data = file_manager.readFromFile("data.txt");
     for (string line : data) {
@@ -83,4 +62,3 @@ int main() {
 
     return 0;
 }
-
