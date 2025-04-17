f09634321568b19d0471cea4520ae439665f939a
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2025-04-17 11:25:34 +0530

    Update 7_constexpr lambda.cpp

--- Modern_CPP_11_14_17_20/C++17/7_constexpr lambda.cpp
+++ Modern_CPP_11_14_17_20/C++17/7_constexpr lambda.cpp
@@ -1,9 +1,14 @@
-Compile-time lambdas using constexpr.
+#constexpr lambda: A constexpr lambda is a lambda function that can be evaluated at compile-time if the input allows.
 
-auto identity = [](int n) constexpr { return n; };
-static_assert(identity(123) == 123);
-constexpr auto add = [](int x, int y) {
-  auto L = [=] { return x; };
-  auto R = [=] { return y; };
-  return [=] { return L() + R(); };
-};
+#include <iostream>
+
+int main() {
+    constexpr auto add = [](int a, int b) {
+        return a + b;
+    };
+
+    constexpr int x = add(2, 3); // compile-time calculation
+
+    std::cout << "x = " << x << '\n'; // x = 5
+}
+
