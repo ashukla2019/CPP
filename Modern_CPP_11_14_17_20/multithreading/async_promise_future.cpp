556041bff2027bdb8ad920a394ea57465ff48912
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2025-04-22 10:54:11 +0530

    Update async_promise_future.cpp

--- Modern_CPP_11_14_17_20/multithreading/async_promise_future.cpp
+++ Modern_CPP_11_14_17_20/multithreading/async_promise_future.cpp
@@ -1,83 +1,44 @@
-#include <iostream>
-#include <thread>
-using namespace std;
-
-//Since x is shared variable b/w parent and child thread. We need to protect it using 
-//mutex and condition variable: First x should be set in child context and should be read in parent
-//context.
-
-void func(int y, int& x)
-{
-    cout<<"y="<<y<<"\n";
-    x = 20;
-}
-
-int main()
-{
-    int x;  
-    thread t(func, 10, ref(x));
-    t.join();
-    cout<<"Main Thread\n";
-    return 0;
-}
-
-//we can use standard library function async instead of thread.
-//future represent objects where you can get something later. 
-//can not call get more than once.
+std::async: runs a function asynchronously (potentially on a separate thread) and returns a std::future to retrieve
+the result.
 
 #include <iostream>
-#include <thread>
 #include <future>
-using namespace std;
 
-int func(int y)
-{
-    cout<<"Child Thread\n";
-    return y*2;
+//With std::launch::async:
+
+//The function runs asynchronously in a separate thread.
+
+//get() will block until the function completes.
+
+//With std::launch::deferred:
+
+//The function runs synchronously(on the calling thread) when get() is called.
+
+//No separate thread is created.
+
+//get() will block until the function finishes, but since it runs on the same thread, the blocking is just 
+//waiting for the function to complete.
+
+//std::future: is an object that acts as a placeholder for a result that will be computed asynchronously.
+
+//.get() retrieves the resultand blocks if not ready.
+//.wait() just blocks until ready(without retrieving the value).
+//.valid() checks if the future still holds a result.
+
+int compute() {
+    return 42;
 }
 
-int main()
-{
-    int x;  
-    
-    future<int> fu = async(launch::deferred, func, 10); //function will be executed in same thread.
-	//future<int> fu = async(launch::async, func, 10); function will be executed in other thread.
-	//future<int> fu = async(launch::async | launch::deferred, func, 10); function will be executed in other thread.
-    x= fu.get(); //will wait until child thread finishes.
-    cout<<"Main Thread\n";
-    
-    return 0;
+int main() {
+    //launch::async forces it to run on a new thread.
+    //std::future<int> result = std::async(std::launch::async, compute);
+    //No force it to run on a new thread.
+    //std::future<int> result = std::async(compute);
+    //std::launch::deferred: delays execution until get() or wait() is called
+    std::future<int> result = std::async(std::launch::deferred, compute);
+    std::cout << "Result: " << result.get() << std::endl;
 }
 
 
-//Use of Promise: pass value from parent thread to child thread, not at the time of
-//creation of thread but later in future.
-
-#include <iostream>
-#include <thread>
-#include <future>
-using namespace std;
-
-//since x is shared variable b/w parent and child thread. We need to protect it using 
-//mutex and condition variable: First x should be set in child context and should be read in parent
-//context.
-
-int func(future<int>& f)
-{
-    int n = f.get();
-    cout<<"Child Thread\n";
-    return n;
-}
-
-int main()
-{
-    int x;  
-    promise<int>p;
-    future<int> f = p.get_future();
-    future<int> fu = async(launch::async, func, ref(f)); 
-    cout<<"Main Thread\n";
-    p.set_value(24);
-    x= fu.get(); 
-    cout<<"value"<<x<<endl;
-    return 0;
-}
+//std::promise is used to communicate data between threads.It allows one thread(the producer) to send a value or
+//an exception to another thread(the consumer) through a std::future
