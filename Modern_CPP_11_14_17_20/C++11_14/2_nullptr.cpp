17f63e92e70bb0ac8a3df7231da5de541beff1e8
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2025-05-12 18:43:11 +0530

    Update 2_nullptr.cpp

--- Modern_CPP_11_14_17_20/C++11_14/2_nullptr.cpp
+++ Modern_CPP_11_14_17_20/C++11_14/2_nullptr.cpp
@@ -1,4 +1,8 @@
 //Nullptr:To avoid mistakes which might occur when a null pointer gets interpreted as an integral value. 
+Type-Safety
+nullptr is of type std::nullptr_t
+It can only convert to pointer types, not integers
+
 void fun(char* s)
 {
 	cout<<"inside fun(char*)"<<endl;
