021e6a400f63a071aa6b9c2acbcfb047c774747d
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2025-07-16 14:58:38 +0530

    Update smart_pointer_implemetation.cpp

--- C++_concepts/Smart_pointers/smart_pointer_implemetation.cpp
+++ C++_concepts/Smart_pointers/smart_pointer_implemetation.cpp
@@ -58,3 +58,47 @@ int main()
     p->Display();
     //delete pPerson;
 }
+-------------------------------------------------------------------------------------------------------------------  
+#include <iostream>
+#include<tuple>
+#include <memory>
+
+using namespace std;
+
+class user
+{
+	int user_id;
+	string user_name;
+public:
+	user()=default;
+	user(int id, string name):user_id(id), user_name(name){
+	}
+	pair<int, string>getUserDetails(){
+		return make_pair(user_id, user_name);
+	}
+};
+
+template<typename T>
+class smart_ptr{
+	T* usr;
+public:
+	smart_ptr()=default;
+	smart_ptr(T* t):usr(t){
+	}
+	T* operator->(){
+		return usr;
+	}
+	~smart_ptr(){
+		if (usr)
+			delete usr;
+	}
+};
+
+int main(){
+	string s("Ankit");
+	//user u(1, s);
+	smart_ptr<user>u(new user(1, s));
+	auto [id, name] = u->getUserDetails(); 
+	cout<<"user-id="<<id<<" "<<"user_name="<<name<<"\n";
+	return 0;
+}
